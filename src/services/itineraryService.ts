c2c406df49b64f7b5123f7c24283e4abda2903f8
tobiasburman <tobias.burman99@gmail.com>
2026-02-19 15:41:12 +0100

    Add calculateTotalCost, addActivity, and createTrip

--- src/services/itineraryService.ts
+++ src/services/itineraryService.ts
@@ -1,5 +1,27 @@
-import type { Activity, Trip } from './../models';
+import type { Activity, Trip } from "../models.js";
 
+//Calculate total cost
 export const calculateTotalCost = (trip: Trip): number => {
     return trip.activities.reduce((sum, activity)=> sum + activity.cost, 0)
 };
+
+//add activity
+export const addActivity = (trip: Trip, activity: Activity): Trip => {
+    const newActivities = [...trip.activities, activity]; 
+  
+  return {
+    ...trip,
+    activities: newActivities
+  };
+};
+
+//Create a trip
+export const createTrip = (destination: string, startDate: Date): Trip => {
+    const id = Date.now().toString(); //Konverterar nuvarande tid i millesekunder till en sträng
+    return {
+        id,
+        destination,
+        startDate,
+        activities: []
+      };
+    };
