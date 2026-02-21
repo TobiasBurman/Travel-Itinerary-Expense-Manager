147f9f374718153a500aafa32cfc5d4bd64f8c85
dimuthu-mad <dimuthumaduranga96@gmail.com>
2026-02-21 19:56:35 +0100

    get country data from api

--- src/services/destinationServices.ts
+++ src/services/destinationServices.ts
@@ -1,13 +1,15 @@
 import type { Activity, Trip } from "../models.js";
 
 export const getDestinationInfo = async (countryName: string) => {
-  const countryName2 = "sri lanka";
+  const countryName2 = "sweden";
   try {
     const response = await fetch(
       `https://restcountries.com/v3.1/name/${countryName2}`,
     );
     const data = await response.json();
-    console.log(data);
+    console.log(data[0].flags.png);
+    console.log(data[0]);
+
     return {
       currency: Object.keys(data[0].currencies)[0],
       flag: data[0].flag,
@@ -16,3 +18,5 @@ export const getDestinationInfo = async (countryName: string) => {
     throw new Error("Could not fetch country data");
   }
 };
+
+getDestinationInfo("sweden");
